7e7360a82f4b951dad90ec2f25b04ec2c1575de3
nrdsstuff <nrdfoss@gmail.com>
2024-03-28 09:41:36 +0100

    removed garbage

--- universal/syncthing.nix
+++ universal/syncthing.nix
@@ -4,8 +4,7 @@
     services.syncthing = {
         enable = true;
         user = "nrd";
-        dataDir = "/home/nrd/SyncthingDefault";    # Default folder for new synced folders
-        configDir = "/home/nrd/SyncthingDefault/.config/syncthing";   # Folder for Syncthing's settings and keys
+        configDir = "/home/nrd/.config/syncthing";   # Folder for Syncthing's settings and keys
     };
     
     services.syncthing.settings.devices = {
