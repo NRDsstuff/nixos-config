93476c7509c4faa5f697ff4f954e3c0efc8fe4db
nrdsstuff <nrdfoss@gmail.com>
2024-04-21 19:38:08 +0200

    cleanup :3333333333

--- desktop/boot.nix
+++ desktop/boot.nix
@@ -1,32 +1,32 @@
 { config, pkgs, ... }:
 
 { 
-    # kernel modules
     boot = {
+        # kernel modules
         initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
         supportedFilesystems = [ "ntfs" ];
         initrd.kernelModules = [ ];
         kernelModules = [ "kvm-intel" ];
         extraModulePackages = [ ];
         kernelPackages = pkgs.linuxPackages_latest; 
-    };
-    
-    # bootloader  
-    boot.loader = {
-        systemd-boot = {
-            enable = false;
-            memtest86.enable = true;
-            consoleMode = "max";
-        };
-        efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot"; 
-        };
-        grub = {
-            devices = ["nodev"];  # Despite what the manpage indicates, setting device to "nodev" will still call grub-install if efiSupport is true
-            efiSupport = true;
-            enable = true;
-            gfxmodeEfi = "1920x1080";
+
+        # bootloader
+        loader = {
+            systemd-boot = {
+                enable = false;
+                memtest86.enable = true;
+                consoleMode = "max";
+            };
+            efi = {
+                canTouchEfiVariables = true;
+                efiSysMountPoint = "/boot"; 
+            };
+            grub = {
+                devices = ["nodev"];  # Despite what the manpage indicates, setting device to "nodev" will still call grub-install if efiSupport is true
+                efiSupport = true;
+                enable = true;
+                gfxmodeEfi = "1920x1080";
+            };
         };
     };
 }
