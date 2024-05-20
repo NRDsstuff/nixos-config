8d34e17707578b5643698d59138c681d25d2eea2
nrdsstuff <nrdfoss@gmail.com>
2024-05-20 21:13:45 +0200

    hiding another app menu entry

--- users/gui/nrd/dconf.nix
+++ users/gui/nrd/dconf.nix
@@ -131,7 +131,11 @@
 
             # app hider
             "org/gnome/shell/extensions/app-hider" = {
-                hidden-apps = ["cups.desktop" "chromium-browser.desktop" "nixos-manual.desktop" "nvidia-settings.desktop" "yelp.desktop" "xterm.desktop" "simple-scan.desktop" "Steam Linux Runtime 3.0 (sniper).desktop" "Proton Experimental.desktop"];
+                hidden-apps = [ "cups.desktop" "chromium-browser.desktop" "nixos-manual.desktop" "nvidia-settings.desktop" "yelp.desktop" "xterm.desktop" "simple-scan.desktop" "Steam Linux Runtime 3.0 (sniper).desktop" "Proton Experimental.desktop" "FFPWA-01HTCDY9B7AXS7NHSWW5FG19G7.desktop" ];
+            };
+
+            "org/gnome/shell/extensions/app-hider/hidden-apps" = {
+                hidden-apps = [ "cups.desktop" "chromium-browser.desktop" "nixos-manual.desktop" "nvidia-settings.desktop" "yelp.desktop" "xterm.desktop" "simple-scan.desktop" "Steam Linux Runtime 3.0 (sniper).desktop" "Proton Experimental.desktop" ];
             };
 
             # mpris
